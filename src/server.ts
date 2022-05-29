b799d89c344fbc8edfecee91dde6fb178c4b637c
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-05-28 21:17:39 -0400

    setup typeorm db connection

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,25 @@
 // NPM Modules
 const dotenv = require('dotenv');
+const tsDBConnect = require('typeorm');
+require('reflect-metadata');
 // Enable Config File
 dotenv.config({
     path: './config.env'
 });
+// Connect to MySQL DB
+tsDBConnect
+    .createConnection({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        logging: false,
+        entities: [User]
+    })
+    .then(() => {
+        // here you can start to work with your entities
+        console.log('MySQL Database Connection Status: success');
+    })
+    .catch((error: any) => console.log(error));
