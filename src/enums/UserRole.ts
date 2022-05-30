b8b9b2133e474a67292c7fb4167e6279d12e2e8c
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-05-29 22:06:26 -0400

    convert class to enum

--- src/enums/UserRole.ts
+++ src/enums/UserRole.ts
@@ -1,21 +1,10 @@
-export class UserRole {
-    EmployeeDevFrontEnd: string;
-    EmployeeDevBackEnd: string;
-    EmployeeDatabaseAdmin: string;
-    EmployeeCustomerService: string;
-    EmployeeTechSupport: string;
-    EmployeeAdmin: string;
-    EmployeeSuperAdmin: string;
-    Customer: string;
-
-    constructor() {
-        this.EmployeeDevFrontEnd = 'employee-dev-front-end';
-        this.EmployeeDevBackEnd = 'employee-dev-back-end';
-        this.EmployeeDatabaseAdmin = 'employee-database-admin';
-        this.EmployeeCustomerService = 'employee-customer-service';
-        this.EmployeeTechSupport = 'employee-technical-support';
-        this.EmployeeAdmin = 'employee-admin';
-        this.EmployeeSuperAdmin = 'employee-super-admin';
-        this.Customer = 'customer';
-    }
+export enum UserRole {
+    EmployeeDevFrontEnd = 'employee-dev-front-end',
+    EmployeeDevBackEnd = 'employee-dev-back-end',
+    EmployeeDatabaseAdmin = 'employee-database-admin',
+    EmployeeCustomerService = 'employee-customer-service',
+    EmployeeTechSupport = 'employee-technical-support',
+    EmployeeAdmin = 'employee-admin',
+    EmployeeSuperAdmin = 'employee-super-admin',
+    Customer = 'customer'
 }
