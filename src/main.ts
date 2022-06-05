1bd302d04c301168c654171e02290480f12f866c
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-05 19:56:45 -0400

    setup url prefix for api endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true
+        })
+    );
+    app.setGlobalPrefix('api/v1', {
+        exclude: [
+            '/',
+            'about',
+            'ads',
+            'contact',
+            'legal/terms-conditions',
+            'legal/privacy-policy',
+            'legal/shipping-policy',
+            'legal/return-policy',
+            'login',
+            'logout',
+            'press',
+            'register'
+        ]
+    });
+    await app.listen(3333);
 }
 bootstrap();
