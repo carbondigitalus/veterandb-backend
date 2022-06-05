fadaa78cd95a95414c2ccfc11dba26e09d9249e0
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-05 14:57:12 -0400

    create test for enum UserRole

--- test/backend-unit/core/enums.test.ts
+++ test/backend-unit/core/enums.test.ts
@@ -4,7 +4,7 @@ import chaiAsPromised from 'chai-as-promised';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import 'mocha';
-
+import { AccountStatus, UserRole } from '../../../src/enums';
 // Module Extenders
 const { expect } = chai;
 chai.use(chaiAsPromised);
@@ -16,19 +16,48 @@ describe('Node Core Tests', () => {
         sandbox.restore();
     });
 
-    context('Env Tests', () => {
-        it('Node Version: should be v16.x or greater', (done) => {
-            const versionRaw = process.version;
-            const versionMajor = versionRaw.split('v')[1];
-            const versionNumber = parseInt(versionMajor);
-            expect(versionNumber).to.be.a('number');
-            expect(versionNumber).to.be.at.least(16);
+    context('Core Enums Tests', () => {
+        it('Enum: User Role', (done) => {
+            // check each specific value
+            expect(UserRole.Customer).to.include('customer');
+            expect(UserRole.EmployeeAdmin).to.include('employee-admin');
+            expect(UserRole.EmployeeCustomerService).to.include(
+                'employee-customer-service'
+            );
+            expect(UserRole.EmployeeDatabaseAdmin).to.include(
+                'employee-database-admin'
+            );
+            expect(UserRole.EmployeeDevBackEnd).to.include(
+                'employee-dev-back-end'
+            );
+            expect(UserRole.EmployeeDevFrontEnd).to.include(
+                'employee-dev-front-end'
+            );
+            expect(UserRole.EmployeeSuperAdmin).to.include(
+                'employee-super-admin'
+            );
+            expect(UserRole.EmployeeTechSupport).to.include(
+                'employee-technical-support'
+            );
+            // create array from options
+            const role: string[] = Object.keys(UserRole).map((item) => {
+                return item;
+            });
+
+            expect(role).to.have.lengthOf(8);
             done();
         });
-        it('Node Env: should be development', (done) => {
-            const nodeENV = process.env.NODE_ENV;
-            expect(nodeENV).to.equal('development');
-            done();
-        });
+        // it('Enum: Account Status', (done) => {
+        //     // Active = 'active',
+        //     // Inactive = 'inactive',
+        //     // Hold = 'hold',
+        //     // Archived = 'archvied'
+        //     const versionRaw = process.version;
+        //     const versionMajor = versionRaw.split('v')[1];
+        //     const versionNumber = parseInt(versionMajor);
+        //     expect(versionNumber).to.be.a('number');
+        //     expect(versionNumber).to.be.at.least(16);
+        //     done();
+        // });
     });
 });
