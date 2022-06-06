dbbca4b08dd1fcc27c5a17104d3244c07d5e3e5f
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-05 22:20:24 -0400

    import jwt guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,3 +1,4 @@
 import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from '../auth/decorator';
+import { JWTGuard } from '../auth/guard';
