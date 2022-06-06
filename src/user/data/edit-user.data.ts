d7dc0e64c453430ca5ca17b3880f77ee2cfd35fa
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-05 23:38:17 -0400

    add comments for clarity

--- src/user/data/edit-user.data.ts
+++ src/user/data/edit-user.data.ts
@@ -1,5 +1,8 @@
+// NPM Module
 import { IsEmail, IsOptional, IsString } from 'class-validator';
 
+// create edit user data class
+// used for validating data is being manipulated correctly
 export class EditUserData {
     @IsEmail()
     @IsOptional()
