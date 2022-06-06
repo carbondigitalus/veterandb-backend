c3d678556fd974e1cffc73f692543ffa5790da46
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-06 01:18:27 -0400

    Create email.d.ts

---
+++ types/email.d.ts
@@ -0,0 +1,9 @@
+namespace NodeJS {
+    export interface ProcessEnv {
+        EMAIL_HOST: string;
+        EMAIL_PORT: number;
+        EMAIL_USERNAME: string;
+        EMAIL_PASSWORD: string;
+        EMAIL_FROM: string;
+    }
+}
