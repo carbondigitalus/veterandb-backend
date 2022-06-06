e1477332f4433cc6e0d120056f40648ea5f77e5d
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-05 22:20:36 -0400

    import data

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,3 +2,4 @@ import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from '../auth/decorator';
 import { JWTGuard } from '../auth/guard';
+import { EditUserDto } from './data';
