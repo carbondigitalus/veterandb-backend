53c273cc23c04554392e4d11b486d18530d79c21
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-05 23:55:36 -0400

    add comments for clarity

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,31 +1,39 @@
+// NPM Modules
 import { ForbiddenException, Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
-import { AuthData } from './data';
-import * as argon from 'argon2';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
-import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import * as argon from 'argon2';
 
+// Custom Modules
+import { AuthData } from './data';
+import { PrismaService } from '../prisma/prisma.service';
+
+// create injectable auth service
 @Injectable()
 export class AuthService {
+    // create constructor
+    // import prisma service, jwt service and cofig service
     constructor(
         private prisma: PrismaService,
         private jwt: JwtService,
         private config: ConfigService
     ) {}
 
+    // create register method
     async register(data: AuthData) {
         // generate the password hash
         const hash = await argon.hash(data.password);
         // save the new user in the db
         try {
+            // create user
             const user = await this.prisma.user.create({
                 data: {
                     email: data.email,
                     hash
                 }
             });
-
+            // return signed token after user creation
             return this.signToken(user.id, user.email);
         } catch (error) {
             if (error instanceof PrismaClientKnownRequestError) {
@@ -37,6 +45,7 @@ export class AuthService {
         }
     }
 
+    // create login method
     async login(data: AuthData) {
         // find the user by email
         const user = await this.prisma.user.findUnique({
@@ -51,24 +60,28 @@ export class AuthService {
         const pwMatches = await argon.verify(user.hash, data.password);
         // if password incorrect throw exception
         if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+        // return signed token if login successful
         return this.signToken(user.id, user.email);
     }
 
+    // create sign token method
     async signToken(
         userId: number,
         email: string
     ): Promise<{ access_token: string }> {
+        // capture jwt payload from user details
         const payload = {
             sub: userId,
             email
         };
+        // get jwt secret from env variable
         const secret = this.config.get('JWT_SECRET');
-
+        // create jwt token
         const token = await this.jwt.signAsync(payload, {
             expiresIn: '15m',
             secret: secret
         });
-
+        // return access token
         return {
             access_token: token
         };
