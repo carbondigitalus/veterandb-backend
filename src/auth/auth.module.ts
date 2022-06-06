8b45376add7b1173e0259c4504fd145ca60e2998
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-05 22:33:20 -0400

    import jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,3 +2,5 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JWTStrategy } from './strategy';
+
