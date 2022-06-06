44c70e82afdb19a3bcd730213282aead01456b31
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-05 22:31:10 -0400

    create auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,3 +2,6 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthData } from './data';
 
+@Controller('users')
+export class AuthController {
+}
