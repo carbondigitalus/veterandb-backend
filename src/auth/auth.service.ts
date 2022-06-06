02c5a85b0d6b9c18cf9abe8134c5bf2bce4f3490
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-05 22:38:54 -0400

    create injectable auth service class

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,3 +5,7 @@ import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AuthService {
+}
