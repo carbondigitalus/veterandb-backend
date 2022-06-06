4fa3d6a5604cbde532b5a03f06d5986a6a537789
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-05 22:40:33 -0400

    create sign token method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -54,4 +54,23 @@ export class AuthService {
         return this.signToken(user.id, user.email);
     }
 
+    async signToken(
+        userId: number,
+        email: string
+    ): Promise<{ access_token: string }> {
+        const payload = {
+            sub: userId,
+            email
+        };
+        const secret = this.config.get('JWT_SECRET');
+
+        const token = await this.jwt.signAsync(payload, {
+            expiresIn: '15m',
+            secret: secret
+        });
+
+        return {
+            access_token: token
+        };
+    }
 }
