c67068772436e89894d3923d644418186876853b
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-05 22:26:05 -0400

    create jwt strategy class

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -4,3 +4,6 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PrismaService } from '../../prisma/prisma.service';
 
+@Injectable()
+export class JWTStrategy extends PassportStrategy(Strategy, 'jwt') {
+}
