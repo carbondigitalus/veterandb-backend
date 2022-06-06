88574029330d632cba50ea6ba802f4c239936ab1
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-05 22:29:05 -0400

    update get user functionality

--- src/auth/decorator/get-user.decorator.ts
+++ src/auth/decorator/get-user.decorator.ts
@@ -8,5 +8,12 @@ export const GetUser = createParamDecorator(
     data: string | undefined,
     ctx: ExecutionContext,
   ) => {
+    const request: Express.Request = ctx
+      .switchToHttp()
+      .getRequest();
+    if (data) {
+      return request.user[data];
+    }
+    return request.user;
   },
 );
