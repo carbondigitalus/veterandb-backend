f6345598881baabd34127b6c7e8ad28f0bff3c4f
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-06 00:00:36 -0400

    add comments for clarity

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,34 @@
+// NPM Modules
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+
+// Custom Modules
 import { AuthService } from './auth.service';
 import { AuthData } from './data';
 
+// create controller with decorator
+// create auth controller class with /users endpoint
 @Controller('users')
 export class AuthController {
+    // create constructor
+    // import auth service data
     constructor(private authService: AuthService) {}
 
+    // create post request with /users/login endpoint
+    // create login function
+    // capture req.body in data variable
     @HttpCode(HttpStatus.OK)
     @Post('login')
-    signin(@Body() data: AuthData) {
-        return this.authService.signin(data);
+    login(@Body() data: AuthData) {
+        // use login function with captured data
+        return this.authService.login(data);
     }
+
+    // create post request with /users/register endpoint
+    // create register function
+    // capture req.body in data variable
     @Post('register')
-    signup(@Body() data: AuthData) {
-        return this.authService.signup(data);
+    register(@Body() data: AuthData) {
+        // use register function with captured data
+        return this.authService.register(data);
     }
 }
