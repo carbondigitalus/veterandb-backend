8fb43eb6ab9905b72a4f6c493908e4225bd7485e
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-06 00:04:58 -0400

    add comments for clarity

--- src/auth/guard/jwt.guard.ts
+++ src/auth/guard/jwt.guard.ts
@@ -1,5 +1,7 @@
+// NPM Modules
 import { AuthGuard } from '@nestjs/passport';
 
+// create jwt guard class
 export class JWTGuard extends AuthGuard('jwt-guard') {
     constructor() {
         super();
