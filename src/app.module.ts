888a2607791981becca48699a869406871239342
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-06 01:12:59 -0400

    initial typeorm setup, pending connection data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 // NPM Modules
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 // create module from decorator
 @Module({
-    imports: [],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'root',
+            database: 'test',
+            entities: [],
+            synchronize: true
+        })
+    ],
     controllers: [],
     providers: []
 })
