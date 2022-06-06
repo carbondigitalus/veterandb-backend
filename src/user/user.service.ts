79070465ab4e41b64c06cddf0e0ffc76c4624e10
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-05 22:17:53 -0400

    create injectable class user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,3 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { EditUserDto } from './data';
+
+@Injectable()
+export class UserService {
+}
