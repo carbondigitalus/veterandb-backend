cee04dc725a04827258b27a23ff294c91d5cd6da
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-05 22:40:15 -0400

    create register

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,4 +14,27 @@ export class AuthService {
         private config: ConfigService
     ) {}
 
+    async register(data: AuthData) {
+        // generate the password hash
+        const hash = await argon.hash(data.password);
+        // save the new user in the db
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: data.email,
+                    hash
+                }
+            });
+
+            return this.signToken(user.id, user.email);
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials taken');
+                }
+            }
+            throw error;
+        }
+    }
+
 }
