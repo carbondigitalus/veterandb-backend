d43ee0d691a47469664e94946599cf5c7f88ec9b
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-05 23:57:21 -0400

    add comments for clarity

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
+// NPM Modules
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+
+// Custom Modules
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JWTStrategy } from './strategy';
 
+// create module decorator
+// create class auth module
 @Module({
     imports: [JwtModule.register({})],
     controllers: [AuthController],
