17dfe0b369d11b2fbb77aa0e405b5ac71b998209
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-05 22:38:09 -0400

    import prisma as placeholder

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,3 +2,4 @@ import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthData } from './data';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
