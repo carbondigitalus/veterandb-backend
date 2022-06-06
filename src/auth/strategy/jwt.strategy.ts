c0c229ab3b56e7f32d5f3b9a17e826a18b33ea8d
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-06 00:03:52 -0400

    add comments for clarity

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,11 +1,17 @@
+// NPM Modules
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+
+// Custom Modules
 import { PrismaService } from '../../prisma/prisma.service';
 
+// create injectable jwt strategy class
 @Injectable()
 export class JWTStrategy extends PassportStrategy(Strategy, 'jwt') {
+    // create constructor
+    // import config service and prisma service data
     constructor(config: ConfigService, private prisma: PrismaService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -13,12 +19,17 @@ export class JWTStrategy extends PassportStrategy(Strategy, 'jwt') {
         });
     }
 
+    // create validate method
+    // use jwt payload, user data and email
     async validate(payload: { sub: number; email: string }) {
+        // find user
         const user = await this.prisma.user.findUnique({
             where: {
                 id: payload.sub
             }
         });
+        // delete user hash
+        // return user
         delete user.hash;
         return user;
     }
