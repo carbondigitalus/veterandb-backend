197fde666f958a9dd1869e81e1102f58efe79d9c
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-05 22:37:51 -0400

    import argon2

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,4 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthData } from './data';
+import * as argon from 'argon2';
