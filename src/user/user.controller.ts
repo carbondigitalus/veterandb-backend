9895632209173c242b98a0a11f9455ba43fdcf08
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-05 23:51:35 -0400

    add comments for clarity

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,21 +1,34 @@
+// NPM Modules
 import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
+
+// Custom Modules
 import { GetUser } from '../auth/decorator';
 import { JWTGuard } from '../auth/guard';
-import { EditUserDto } from './data';
+import { EditUserData } from './data';
 import { UserService } from './user.service';
 
+// setup all subsequent controller methods to access jwt guard
+// create controller with /users endpoint
+// create user controller class
 @UseGuards(JWTGuard)
 @Controller('users')
 export class UserController {
+    // create constructor
+    // access user service data
     constructor(private userService: UserService) {}
+
+    // create get request for /users/me
+    // get user formatted as user
     @Get('me')
     getMe(@GetUser() user: User) {
         return user;
     }
 
+    // create patch request for /users
+    // edit user id and user data
     @Patch()
-    editUser(@GetUser('id') userId: number, @Body() data: EditUserDto) {
+    editUser(@GetUser('id') userId: number, @Body() data: EditUserData) {
         return this.userService.editUser(userId, data);
     }
 }
