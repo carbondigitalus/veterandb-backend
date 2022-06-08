fef9b87f0a24d108d4cd53f0f8a6f527d8277f8b
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-08 00:39:27 -0400

    moved into utils folder

---
+++ src/utils/enums/UserRole.ts
@@ -0,0 +1,10 @@
+export enum UserRole {
+    EmployeeDevFrontEnd = 'employee-dev-front-end',
+    EmployeeDevBackEnd = 'employee-dev-back-end',
+    EmployeeDatabaseAdmin = 'employee-database-admin',
+    EmployeeCustomerService = 'employee-customer-service',
+    EmployeeTechSupport = 'employee-technical-support',
+    EmployeeAdmin = 'employee-admin',
+    EmployeeSuperAdmin = 'employee-super-admin',
+    Customer = 'customer'
+}
