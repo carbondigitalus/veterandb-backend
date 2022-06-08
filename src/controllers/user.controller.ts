dade2f1779245e45b77767019c2be8da65265508
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-08 18:42:23 -0400

    move all account routes into account controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -22,38 +22,6 @@ export class UserController {
     // access user service data
     constructor() {}
 
-    // route: /users/account-deactivate
-    @Delete('account-deactivate')
-    accountDeactivate() {}
-
-    // route: /users/account-login
-    @Post('account-login')
-    accountLogin() {}
-
-    // route: /users/account-logout
-    @Get('account-logout')
-    accountLogout() {}
-
-    // route: /users/account-password-forgot
-    @Post('account-password-forgot')
-    accountPasswordForgot() {}
-
-    // route: /users/account-password-reset/:id
-    @Patch('account-password-reset/:token')
-    accountPasswordReset() {}
-
-    // route: /users/account-register
-    @Post('account-register')
-    accountRegister() {}
-
-    // route: /users/account-update-profile
-    @Patch('account-update-profile')
-    accountUpdateProfile() {}
-
-    // route: /users/account-update-settings
-    @Patch('account-update-settings')
-    accountUpdateSettings() {}
-
     // route: /users
     @Post()
     userCreateOne() {}
