a00b7fc91b4f1ac4c1f431263bd3cac2e1737299
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-08 00:40:22 -0400

    moved into utils folder

--- src/guards/jwt.guard.ts
+++
@@ -1,9 +0,0 @@
-// NPM Modules
-import { AuthGuard } from '@nestjs/passport';
-
-// create jwt guard class
-export class JWTGuard extends AuthGuard('jwt-guard') {
-    constructor() {
-        super();
-    }
-}
