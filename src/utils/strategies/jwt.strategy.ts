314194296470d5dfa9fe06297847ed1f80a019c9
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-08 00:39:08 -0400

    moved into utils folder

---
+++ src/utils/strategies/jwt.strategy.ts
@@ -0,0 +1,34 @@
+// NPM Modules
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+// Custom Modules
+
+// create injectable jwt strategy class
+@Injectable()
+export class JWTStrategy extends PassportStrategy(Strategy, 'jwt') {
+    // create constructor
+    // import config service and prisma service data
+    constructor(private userData: any) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: process.env.JWT_SECRET
+        });
+    }
+
+    // create validate method
+    // use jwt payload, user data and email
+    async validate(payload: { sub: number; email: string }) {
+        // find user
+        const user = await this.userData.user.findUnique({
+            where: {
+                id: payload.sub
+            }
+        });
+        // delete user hash
+        // return user
+        delete user.hash;
+        return user;
+    }
+}
