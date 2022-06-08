97244b6e5320de0530824694ba8fc94a6d427d03
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-08 00:39:15 -0400

    moved into utils folder

---
+++ src/utils/models/edit-user.model.ts
@@ -0,0 +1,18 @@
+// NPM Module
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+// create edit user data class
+// used for validating data is being manipulated correctly
+export class EditUserData {
+    @IsEmail()
+    @IsOptional()
+    email?: string;
+
+    @IsString()
+    @IsOptional()
+    firstName?: string;
+
+    @IsString()
+    @IsOptional()
+    lastName?: string;
+}
