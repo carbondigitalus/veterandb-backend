a7a95bc9d17c50e85b842f5e733a63393bdab797
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-13 19:33:12 -0400

    import TypeORM module

--- src/modules/account.module.ts
+++ src/modules/account.module.ts
@@ -1,6 +1,7 @@
 // NPM Modules
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 // Custom Modules
 import { AccountController } from '../controllers';
