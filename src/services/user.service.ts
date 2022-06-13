a45f80af46978ee9fb739c91e0f6699435c9d511
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-13 19:35:21 -0400

    import Repository

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,7 @@
 // NPM Modules
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 // Custom Modules
 import { EditUserData } from './../utils';
 
