48cd469f09a4e4b2ed8ea4794b3ad8a146a31675
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-16 00:04:53 -0400

    apply injectable decorator

--- src/utils/guards/jwt.guard.ts
+++ src/utils/guards/jwt.guard.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from '@nestjs/passport';
 
 // create jwt guard class
 export class JWTGuard extends AuthGuard('jwt-guard') {
+@Injectable()
     constructor() {
         super();
     }
