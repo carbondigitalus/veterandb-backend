139c9c549fb6d21c92b96f7af9d6cf43082e46b4
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-16 23:16:06 -0400

    add nonprofit entity to barrel

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,5 @@
 export { default as Business } from './business.entity';
 export { default as Directory } from './directory.entity';
 export { default as FAQ } from './faq.entity';
+export { default as NonProfit } from './nonprofit.entity';
 export { default as User } from './user.entity';
