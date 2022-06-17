0b9321d3dd484de7e34514f84712b04eaeed65d1
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-16 23:16:15 -0400

    add podcast entity to barrel

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,4 +2,5 @@ export { default as Business } from './business.entity';
 export { default as Directory } from './directory.entity';
 export { default as FAQ } from './faq.entity';
 export { default as NonProfit } from './nonprofit.entity';
+export { default as Podcast } from './podcast.entity';
 export { default as User } from './user.entity';
