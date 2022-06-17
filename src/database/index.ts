199607cd04a76e9ad4c2da8d4e4fc2f4ae6180db
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-16 23:15:57 -0400

    add faq entity to barrel

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,4 @@
 export { default as Business } from './business.entity';
 export { default as Directory } from './directory.entity';
+export { default as FAQ } from './faq.entity';
 export { default as User } from './user.entity';
