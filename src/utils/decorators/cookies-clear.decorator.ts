171c535beb6ee9bd38ee6503a3bece1d75c7eb9f
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-20 23:02:36 -0400

    create clear cookies decorator from nestjsplus/cookies repo

---
+++ src/utils/decorators/cookies-clear.decorator.ts
@@ -0,0 +1,20 @@
+import { UseInterceptors, SetMetadata } from '@nestjs/common';
+import { ClearCookiesInterceptor } from './../interceptors';
+
+/**
+ * Request method decorator.  Causes decorated method to clear
+ * all named cookies.
+ *
+ * @param cookies List of cookie names to be cleared.
+ *
+ */
+export function ClearCookies(...cookies: string[]) {
+    return (target, propertyKey, descriptor) => {
+        SetMetadata('cookieNames', cookies)(target, propertyKey, descriptor);
+        UseInterceptors(ClearCookiesInterceptor)(
+            target,
+            propertyKey,
+            descriptor
+        );
+    };
+}
