8ca98158f2f5c6942d98ac451875127a799d9f17
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-20 23:03:04 -0400

    update file name in barrel

--- src/utils/decorators/index.ts
+++ src/utils/decorators/index.ts
@@ -1,4 +1,4 @@
-export * from './get-cookies.decorator';
+export * from './cookies-get.decorator';
 export * from './get-user.decorator';
 export * from './routes-protected.decorator';
 export * from './validator-match.decorator';
