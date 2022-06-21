1f963cc05a755ba3429526a00802278cffe9a8f8
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-21 02:07:24 -0400

    add request object into login method

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -64,7 +64,7 @@ export class AccountService {
     }
 
     // create login method
-    async login(data: AccountLoginDTO) {
+    async login(data: AccountLoginDTO, req: Request) {
         // find the user by email
         const user = await this.userRepository.findOne({
             where: {
