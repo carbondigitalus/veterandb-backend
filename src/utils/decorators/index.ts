c1b177d5f42d66700bf19d310c913304ec2c91ad
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-21 02:06:50 -0400

    update barrel with cookies-set

--- src/utils/decorators/index.ts
+++ src/utils/decorators/index.ts
@@ -1,5 +1,6 @@
 export * from './cookies-clear.decorator';
 export * from './cookies-get.decorator';
+export * from './cookies-set.decorator';
 export * from './get-user.decorator';
 export * from './routes-protected.decorator';
 export * from './validator-match.decorator';
