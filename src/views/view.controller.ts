48cdbfd2fc7904376b564841fdc10b52e6bf1096
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-23 01:29:37 -0400

    setup base view module

---
+++ src/views/view.controller.ts
@@ -0,0 +1,20 @@
+// NPM Modules
+import {
+    Controller,
+    Get,
+    HttpCode,
+    HttpStatus,
+    Render,
+    Res
+} from '@nestjs/common';
+
+// Custom Modules
+import { ViewService } from './view.service';
+
+// route: /
+@Controller()
+export class ViewController {
+    // create constructor
+    constructor(private viewService: ViewService) {}
+
+}
