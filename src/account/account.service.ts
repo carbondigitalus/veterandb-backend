71baef195d6ba52e67dee44dae9ad8461601472a
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-23 21:34:27 -0400

    convert env variable to number

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -93,7 +93,7 @@ export class AccountService {
                 options: {
                     expires: new Date(
                         Date.now() +
-                            process.env.JWT_COOKIE_EXPIRES_IN *
+                            Number(process.env.JWT_COOKIE_EXPIRES_IN) *
                                 24 *
                                 60 *
                                 60 *
