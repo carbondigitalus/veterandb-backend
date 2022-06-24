3cd3fd0dac05118b0c3c78f5e6b0d8cd3fd7a6c6
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-23 21:35:15 -0400

    setup correct method for static assets

--- src/main.ts
+++ src/main.ts
@@ -17,8 +17,8 @@ async function bootstrap() {
             whitelist: true
         })
     );
-    app.useStaticAssets(`${__dirname}/public`);
     app.setBaseViewsDir(`${__dirname}/src/views`);
+    app.useStaticAssets(join(__dirname, '..', 'public'));
     // add url prefix with exceptions
     app.setGlobalPrefix('api/v1', {
         exclude: [
