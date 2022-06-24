1a77d5cbfb47eb518c2e6cecda2dd5faf8717adf
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-23 21:34:37 -0400

    convert env variable to number

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ import { ReviewModule } from './review/review.module';
         TypeOrmModule.forRoot({
             type: 'mysql',
             host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
+            port: Number(process.env.DB_PORT),
             username: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_TABLE,
