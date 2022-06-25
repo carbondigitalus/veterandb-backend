190c60fb84c24a965c6129b851f89423081a3206
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-25 18:51:02 -0400

    setup submit handler for login form

--- public/js/index.js
+++ public/js/index.js
@@ -4,4 +4,10 @@ $(document).ready(function () {
     // DOM Elements
     const loginForm = $('#loginForm');
 
+    loginForm.submit((event) => {
+        event.preventDefault();
+        const email = $('#email').val();
+        const password = $('#password').val();
+        login(email, password);
+    });
 });
