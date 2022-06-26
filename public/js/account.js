3f933ff7972b5cab094578d8d054d9ba8d223937
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-26 00:06:17 -0400

    create register function

--- public/js/account.js
+++ public/js/account.js
@@ -24,3 +24,39 @@ export const login = (email, password) => {
             console.log('POST: /api/v1/account/login/ | status: completed');
         });
 };
+
+export const register = (
+    firstName,
+    lastName,
+    email,
+    password,
+    passwordConfirm
+) => {
+    console.log('Begin capture form data.');
+    $.ajax({
+        method: 'post',
+        url: '/api/v1/account/register/',
+        data: {
+            firstName,
+            lastName,
+            email,
+            password,
+            passwordConfirm
+        }
+    })
+        .done(function () {
+            console.log('POST: /api/v1/account/register/ | status: success');
+            setTimeout(() => {
+                window.location.assign('/');
+            }, 3000);
+        })
+        .fail(function (error) {
+            console.log('POST: /api/v1/account/register/ | status: error');
+            console.log(`Error Received: ${error}`);
+            console.dir(error);
+            console.table(error);
+        })
+        .always(function () {
+            console.log('POST: /api/v1/account/register/ | status: completed');
+        });
+};
