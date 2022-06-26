85b9fd18522ad545dd882a197561e803292159b0
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-26 00:06:28 -0400

    add register form functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,24 @@
-import { login } from './account';
+import { login, register } from './account';
 
 $(document).ready(function () {
     // DOM Elements
     const loginForm = $('#loginForm');
+    const registerForm = $('#registerForm');
 
     loginForm.submit((event) => {
         event.preventDefault();
         const email = $('#email').val();
         const password = $('#password').val();
         login(email, password);
+    });
+
+    registerForm.submit((event) => {
+        event.preventDefault();
+        const firstName = $('#firstName').val();
+        const lastName = $('#lastName').val();
+        const email = $('#email').val();
+        const password = $('#password').val();
+        const passwordConfirm = $('#passwordConfirm').val();
+        register(firstName, lastName, email, password, passwordConfirm);
     });
 });
