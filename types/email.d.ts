ab8fe437d397a0cd400d61b9ed4d74d51712d1b5
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-28 19:29:07 -0400

    add declare to type files

--- types/email.d.ts
+++ types/email.d.ts
@@ -1,9 +1,13 @@
-namespace NodeJS {
+declare namespace NodeJS {
     export interface ProcessEnv {
         EMAIL_HOST: string;
         EMAIL_PORT: number;
         EMAIL_USERNAME: string;
         EMAIL_PASSWORD: string;
         EMAIL_FROM: string;
+        EMAIL_HOST_DEV: string;
+        EMAIL_PORT_DEV: number;
+        EMAIL_USERNAME_DEV: string;
+        EMAIL_PASSWORD_DEV: string;
     }
 }
